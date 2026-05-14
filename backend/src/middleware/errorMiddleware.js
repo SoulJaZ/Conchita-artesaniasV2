3e450143bff426cffc6c758a1424931279e7f809
RasSoul <mmagtc1101@gmail.com>
2026-05-14 12:33:20 -0500

    Mejorar Middleware

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -1,13 +1,18 @@
-const errorMiddleware = (
-    err,
-    req,
-    res,
-    next
-)=>{
-    res.status(500).json({
-        success:false,
-        messsage: err.messsage
-    });
-}
+const errorMiddleware = (err, req, res, next) => {
+
+  console.error("ERROR:", err);
+
+  res.status(err.status || 500).json({
+
+    success: false,
+
+    message: err.message || "Error interno del servidor",
+
+    stack:
+      process.env.NODE_ENV === "development"
+        ? err.stack
+        : null
+  });
+};
 
 export default errorMiddleware;
